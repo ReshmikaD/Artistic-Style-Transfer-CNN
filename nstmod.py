82fb0b067f101ee1f2940ccd248c10cdbaabf630
ReshmikaD <54103472+ReshmikaD@users.noreply.github.com>
2020-05-07 14:32:47 +0530

    Add files via upload

---
+++ nstmod.py
@@ -0,0 +1,77 @@
+#This code was partly obtained from the MatConvNet team and is used to load the parameters of the VGG19 model.
+
+import numpy as np
+import tensorflow as tf
+
+import os
+import sys
+from matplotlib.pyplot import imshow
+import matplotlib.pyplot as plt
+import scipy.io
+import scipy.misc
+
+from nstmod import *
+
+class DATA:
+    IMAGE_WIDTH = 400
+    IMAGE_HEIGHT = 300
+    COLOR_CHANNELS = 3
+    NOISE_RATIO = 0.6
+    MEANS = np.array([123.68, 116.779, 103.939]).reshape((1,1,1,3)) 
+    VGG_MODEL = 'imagenet-vgg-verydeep-19.mat' 
+    STYLE_IMAGE = 'images/holi_colour.jpg' 
+    CONTENT_IMAGE = 'images/cat.jpg' 
+    OUTPUT_DIR = 'output/'
+    
+def vgg_m(path):
+    vgg = scipy.io.loadmat(path)
+    vgg_layers = vgg['layers']
+    def _weights(layer, expected_layer_name):
+        wb = vgg_layers[0][layer][0][0][2]
+        W = wb[0][0]
+        b = wb[0][1]
+        layer_name = vgg_layers[0][layer][0][0][0][0]
+        assert layer_name == expected_layer_name
+        return W, b
+
+        return W, b
+
+    def _relu(conv2d_layer):
+        return tf.nn.relu(conv2d_layer)
+
+    def _conv2d(prev_layer, layer, layer_name):
+        W, b = _weights(layer, layer_name)
+        W = tf.constant(W)
+        b = tf.constant(np.reshape(b, (b.size)))
+        return tf.nn.conv2d(prev_layer, filter=W, strides=[1, 1, 1, 1], padding='SAME') + b
+
+    def _conv2d_relu(prev_layer, layer, layer_name):
+        return _relu(_conv2d(prev_layer, layer, layer_name))
+
+    def _avgpool(prev_layer):
+        return tf.nn.avg_pool(prev_layer, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+    graph = {}
+    graph['input']   = tf.Variable(np.zeros((1, DATA.IMAGE_HEIGHT, DATA.IMAGE_WIDTH, DATA.COLOR_CHANNELS)), dtype = 'float32')
+    graph['conv1_1']  = _conv2d_relu(graph['input'], 0, 'conv1_1')
+    graph['conv1_2']  = _conv2d_relu(graph['conv1_1'], 2, 'conv1_2')
+    graph['avgpool1'] = _avgpool(graph['conv1_2'])
+    graph['conv2_1']  = _conv2d_relu(graph['avgpool1'], 5, 'conv2_1')
+    graph['conv2_2']  = _conv2d_relu(graph['conv2_1'], 7, 'conv2_2')
+    graph['avgpool2'] = _avgpool(graph['conv2_2'])
+    graph['conv3_1']  = _conv2d_relu(graph['avgpool2'], 10, 'conv3_1')
+    graph['conv3_2']  = _conv2d_relu(graph['conv3_1'], 12, 'conv3_2')
+    graph['conv3_3']  = _conv2d_relu(graph['conv3_2'], 14, 'conv3_3')
+    graph['conv3_4']  = _conv2d_relu(graph['conv3_3'], 16, 'conv3_4')
+    graph['avgpool3'] = _avgpool(graph['conv3_4'])
+    graph['conv4_1']  = _conv2d_relu(graph['avgpool3'], 19, 'conv4_1')
+    graph['conv4_2']  = _conv2d_relu(graph['conv4_1'], 21, 'conv4_2')
+    graph['conv4_3']  = _conv2d_relu(graph['conv4_2'], 23, 'conv4_3')
+    graph['conv4_4']  = _conv2d_relu(graph['conv4_3'], 25, 'conv4_4')
+    graph['avgpool4'] = _avgpool(graph['conv4_4'])
+    graph['conv5_1']  = _conv2d_relu(graph['avgpool4'], 28, 'conv5_1')
+    graph['conv5_2']  = _conv2d_relu(graph['conv5_1'], 30, 'conv5_2')
+    graph['conv5_3']  = _conv2d_relu(graph['conv5_2'], 32, 'conv5_3')
+    graph['conv5_4']  = _conv2d_relu(graph['conv5_3'], 34, 'conv5_4')
+    graph['avgpool5'] = _avgpool(graph['conv5_4'])
+    return graph
